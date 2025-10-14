651581017dc305b3a87a0f3b054207e3c1649492
Randy Fay <randy@randyfay.com>
2025-10-14 23:44:43 +0200

    Get rid of redis for now, just use memcache

--- web/sites/default/settings.upsun.php
+++ web/sites/default/settings.upsun.php
@@ -19,9 +19,9 @@ if (getenv('PLATFORM_PROJECT') != "") {
     ];
 
     // Redis and Memcache settings; just demonstration
-    $settings['redis.connection']['interface'] = 'PhpRedis';
-    $settings['redis.connection']['host'] = getenv('CACHE_HOST');
-    $settings['redis.connection']['port'] = getenv('CACHE_PORT');
+    //$settings['redis.connection']['interface'] = 'PhpRedis';
+    //$settings['redis.connection']['host'] = getenv('CACHE_HOST');
+    //$settings['redis.connection']['port'] = getenv('CACHE_PORT');
 
     $memcache_server = getenv('MEMORY_HOST') . ":" . getenv('MEMORY_PORT');
     $settings['memcache']['servers'] = [ $memcache_server => 'default'];
@@ -32,9 +32,9 @@ if (getenv('PLATFORM_PROJECT') != "") {
     $settings['cache']['bins']['render'] = 'cache.backend.memory';
     $settings['cache']['bins']['dynamic_page_cache'] = 'cache.backend.memory';
 
-    #if (class_exists(\Drupal\redis\Cache\CacheBackendFactory::class)) {
-      $settings['cache']['bins']['bootstrap'] = 'cache.backend.redis';
-      $settings['cache']['bins']['config'] = 'cache.backend.redis';
-    #}
+    // if (class_exists(\Drupal\redis\Cache\CacheBackendFactory::class)) {
+//       $settings['cache']['bins']['bootstrap'] = 'cache.backend.redis';
+//       $settings['cache']['bins']['config'] = 'cache.backend.redis';
+    //}
 
 }
